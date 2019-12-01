30bf678c9bc458af41f0f17798b0aa1a192fcb44
West Kraemer <kraemerwest@gmail.com>
2019-12-01 15:23:14 -0500

    adding another sentence

--- hello.py
+++ hello.py
@@ -6,3 +6,8 @@ age = 20
 name = 'West'
 
 print(f'{name} was {age} years old when he wrote this book')
+
+sport = "soccer"
+skill = "nutmeg"
+
+print(f"West is so nice at {sport}, he {skill}s people all the time")
