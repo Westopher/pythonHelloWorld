16357aeb76091d55637bb46a726f2fc8fa51f73a
West Kraemer <kraemerwest@gmail.com>
2019-12-22 15:42:49 +0100

    add dictionary

--- dataTypes.py
+++ dataTypes.py
@@ -23,7 +23,7 @@ print("my list is now:", shoppingList)
 
  """
 
-zoo = ('python', 'elephant', 'penguin')
+""" zoo = ('python', 'elephant', 'penguin')
 print('Number of animals in the zoo is', len(zoo))
 
 new_zoo = ('monkey', 'camel', zoo)
@@ -33,6 +33,27 @@ print('Animals brought from old zoo are', new_zoo[2])
 print('first animal brought from old zoo is', new_zoo[2][0]) #[2][0] means: third item of new_zoo, first item of that item
 print('Number of animals in the new zoo is',
       len(new_zoo)-1+len(new_zoo[2]))
+ """
+
+ab = {
+    'West': 'west@email.com',
+    'Benedicte': 'benedicte@email.com',
+    'Sky': 'sky@email.com',
+    'Spammer': 'spammer@email.com'
+}
+
+print("West's email is", ab['West'])
+
+
+
+
+
+
+
+
+
+
+
 
 
 
